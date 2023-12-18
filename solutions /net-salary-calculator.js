54e9f45304147264c119d8324450b6913d99f8ff
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-18 12:37:25 +0300

    net-salary-calculator.js update taxf(x) added

--- solutions /net-salary-calculator.js
+++ solutions /net-salary-calculator.js
@@ -1,11 +1,24 @@
-// Basic Salary  and benefits Prompt 
-const basicSalary = Number(prompt("What is your basic salary ? "));
-const benefits = Number(prompt(" What is the total amount of benefits? "));
+// // Basic Salary  and benefits Prompt 
+// const basicSalary = Number(prompt("What is your basic salary ? "));
+// const benefits = Number(prompt(" What is the total amount of benefits? "));
 
 // Gross Salary 
 const grossSalary = basicSalary + benefits;
 
 
-function taxDeduction (grossSalary){
-    console.log(grossSalary);
+function tax(grossPay){
+    let tax = 0;
+    // Tax deductible upto 24000
+    if(grossPay<= 24000){   tax = grossPay * 0.1;}
+    // Tax deductible between 24,001 and 32,333
+    else if(grossPay > 24000 && grossPay <= 32333){    tax = grossPay * 0.25}
+    // Tax deductible between 32,334 and 500,000
+    else if(grossPay > 32333 && grossPay <= 500000){   tax = grossPay * 0.25}
+    // Tax deductible between 500,001 and 800,000
+    else if(grossPay > 500000 && grossPay <= 800000){  tax = grossPay * 0.25}
+    // Tax deductible above 800,000
+    else{ tax = grossPay * 0.35}
+
+    return tax;
 }
+
